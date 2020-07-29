04b0538d272d1edb4a9312dfc5432374cf3e781e
Hemanth Dandu <hemanthme22@gmail.com>
2020-07-29 17:47:58 -0400

    few changes

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,32 +1,27 @@
-import os
+from flask import Flask, request, jsonify
+import pandas as pd
 
-from flask import Flask
+app=Flask(__name__)
 
+@app.route("/upload", methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        print(request.files['file'])
+        f = request.files['file']
+        data_xls = pd.read_excel(f)
+        return data_xls.to_html()
+    return '''
+    <!doctype html>
+    <title>Upload an excel file</title>
+    <h1>Excel file upload (csv, tsv, csvz, tsvz only)</h1>
+    <form action="" method=post enctype=multipart/form-data>
+    <p><input type=file name=file><input type=submit value=Upload>
+    </form>
+    '''
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
+@app.route("/export", methods=['GET'])
+def export_records():
+    return 
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
-
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
-
-    return app
+if __name__ == "__main__":
+    app.run()
